906437d63648925eb941cbde2147feed94130351
Rachael Savoy <rachael.a.hakim@gmail.com>
2021-01-20 14:41:37 +0900

    FEAT: set up components in index file.

--- src/index.jsx
+++ src/index.jsx
@@ -2,21 +2,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-promise';
+import reduxPromise from 'redux-promise';
 
 // internal modules
 import App from './components/app';
 import '../assets/stylesheets/application.scss';
 
 // State and reducers
+import messageReducer from '.reducers/messages_reducer';
+import selectChannelReducer from './reducers/selected_channel_reducer';
+
+const identityReducer = (state = null) => state;
+
+const initialState = {
+  messages: [],
+  channels: ['general', 'react', 'paris'],
+  currentUser: prompt("What is your username?") || `annonymous${Math.floor(10 + (Math.random() * 90))}`,
+  selectedChannel: 'general'
+};
+
 const reducers = combineReducers({
-  changeMe: (state = null, action) => state
+  messages: messagesReducer,
+  channels: indetityReducer,
+  currentUser: identityReducer,
+  selectedChannel: selectedChannelReducer
 });
+
+const middlewares = applyMiddleware(reduxPromise, logger);
+const store = createStore(reducers, initialState, middlewares);
 
 // render an instance of the component in the DOM
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById('app')
 );
